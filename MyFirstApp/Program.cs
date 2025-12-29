1890a406acd5ea0342449e1f48636d835c580043
d7rmo1 <abdalrahmanmohmed64@gmail.com>
2025-12-29 14:47:21 +0000

    First working console app (name and age)

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("ادخل اسمك:");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("ادخل عمرك:");
+        int age = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"مرحبا {name}، عمرك {age} سنة");
+
+        if (age >= 18)
+            Console.WriteLine("أنت بالغ ✅");
+        else
+            Console.WriteLine("أنت قاصر ❌");
+    }
+}
